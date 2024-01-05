974358c678587fcf0ee81f7cc86f51f733e93606
Devansh Behl <heydevanshbehl@gmail.com>
2024-01-05 16:54:30 +0530

    added valid parethesis

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,35 @@
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+
+//https://leetcode.com/problems/valid-parentheses/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else if(s[i] == ')'){
+                if(st.empty()) return false;
+                else if(st.top() != '(') return false;
+                else st.pop();
+            }
+            else if(s[i] == ']'){
+                if(st.empty()) return false;
+                else if(st.top() != '[') return false;
+                else st.pop();
+            }
+            else if(s[i] == '}'){
+                if(st.empty()) return false;
+                else if(st.top() != '{') return false;
+                else st.pop();
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
