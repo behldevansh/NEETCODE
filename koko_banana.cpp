0e91d5f0e657657a8561d8083e17ce73f0ba5cf8
Devansh Behl <heydevanshbehl@gmail.com>
2024-01-09 22:17:46 +0530

    added koko banana

---
+++ koko_banana.cpp
@@ -0,0 +1,27 @@
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+// Return the minimum integer k such that she can eat all the bananas within h hours
+
+//https://leetcode.com/problems/koko-eating-bananas/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+        int minEatingSpeed(vector<int>& piles, int H) {
+        int l = 1, r = 1000000000;
+        while (l < r) {
+            int m = (l + r) / 2, total = 0;
+            for (int p : piles)
+                total += (p + m - 1) / m;
+            if (total > H)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
+    }
+};
